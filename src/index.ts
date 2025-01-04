930ed8c5abf0a37ed9f1f7b68870af4f20b2824a
Yuuzy0721 <adcb20090916@163.com>
2025-01-04 16:52:46 +0800

    初始提交

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Context, Schema } from 'koishi'
+import { Context, Schema, h } from 'koishi'
 
 export const name = 'rtm'
 
@@ -7,5 +7,5 @@ export interface Config {}
 export const Config: Schema<Config> = Schema.object({})
 
 export function apply(ctx: Context) {
-  // write your plugin here
+  // 直链api https://music.163.com/song/media/outer/url?id=537407937.mp3
 }
