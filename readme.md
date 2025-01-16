df6bffc7868cc86efeb60fc526bffdb357a83194
Yuuzy0721 <adcb20090916@163.com>
2025-01-16 16:56:00 +0800

    update

--- readme.md
+++ readme.md
@@ -2,4 +2,4 @@
 
 [![npm](https://img.shields.io/npm/v/koishi-plugin-rtm?style=flat-square)](https://www.npmjs.com/package/koishi-plugin-rtm)
 
-随机东方曲
+随机东方曲，歌曲来自[这个歌单](https://music.163.com/#/playlist?id=2309153779)
--- src/index.ts
+++ src/index.ts
@@ -8,17 +8,13 @@ export interface Config {}
 
 export const Config: Schema<Config> = Schema.object({})
 
-export default Schema.object({
-  returnlog: Schema.boolean().default(false).description('是否在日志中输出歌曲id').experimental(),
-}).description('配置项')
-
 export function apply(ctx: Context) {
   // 直链api https://music.163.com/song/media/outer/url?id=歌曲id.mp3
   ctx.command('rtm', '随机东方曲').alias('随机东方曲').action(async ({ session }) => {
     if (!session) return
     let randomIndex = Math.floor(Math.random() * id.length)
     let randomid = id[randomIndex]
-    console.log(randomid)
+    //console.log('歌曲id' + randomid)
     await session.send(h.audio(`https://music.163.com/song/media/outer/url?id=${randomid}.mp3`))
   })
 }
