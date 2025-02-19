3750edfd2354c07353d1da1192eb73e7bd6f0471
Yuuzy0721 <adcb20090916@163.com>
2025-02-19 21:53:45 +0800

    update

--- readme.md
+++ readme.md
@@ -2,4 +2,4 @@
 
 [![npm](https://img.shields.io/npm/v/koishi-plugin-rtm?style=flat-square)](https://www.npmjs.com/package/koishi-plugin-randomthm)
 
-随机东方曲，歌曲来自[这个歌单](https://music.163.com/#/playlist?id=2309153779)
+随机东方曲，歌曲来自[这个歌单](https://music.163.com/#/playlist?id=2309153779)，如有更新可以发邮件提醒我<adcb20090916@163.com>
--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,45 @@
-import { Context, Schema, h, Session } from 'koishi'
+import { Context, Schema, h } from 'koishi'
 import { id } from './id'
 
 
 export const name = 'randomthm'
 
-export interface Config {}
+export interface Config {
+  SendID: boolean
+  LogOn: boolean
+}
 
-export const Config: Schema<Config> = Schema.object({})
+export const Config: Schema<Config> = Schema.object({
+  SendID: Schema.boolean().description('是否发送音乐id').default(true),
+  LogOn: Schema.boolean().description('是否开启日志').default(false)
+})
 
-export function apply(ctx: Context) {
-  // 直链api https://music.163.com/song/media/outer/url?id=歌曲id.mp3
-  ctx.command('rtm', '随机东方曲').alias('随机东方曲').action(async ({ session }) => {
+export async function apply(ctx: Context, cfg: Config) {
+  // api https://ncm.nekogan.com/song/url/v1?id=歌曲id&level=higher
+  const logger = ctx.logger("randomthm")
+  ctx.command('rtm').alias('随机东方曲').action(async ({ session }) => {
     if (!session) return
-    let randomIndex = Math.floor(Math.random() * id.length)
-    let randomid = id[randomIndex]
-    //console.log('歌曲id' + randomid)
-    await session.send(h.audio(`https://music.163.com/song/media/outer/url?id=${randomid}.mp3`))
+    var randomIndex = Math.floor(Math.random() * id.length)
+    var randomid = id[randomIndex]
+    var url = `https://ncm.nekogan.com/song/url/v1?id=${randomid}&level=higher`
+    //logger.info(url)
+    try{
+      var response = await ctx.http(`https://ncm.nekogan.com/song/url/v1?id=${randomid}&level=higher`)
+      var code = response.data.code
+      var music = response.data.data[0].url
+      if(cfg.SendID === true){
+        await session.send(`音乐id:${randomid}`)
+      }
+      await session.send(h.audio(music))
+    }catch (error){
+      //处理请求错误
+      if(cfg.LogOn === true){
+        logger.error(`请求错误`)
+        logger.info(`歌曲id:${randomid},api地址:${url},状态码:${code},音乐url:${music}`)
+        await session.send('发生错误，请查看日志。')
+      }else{
+        await session.send('发生错误。')
+      }
+    }
   })
 }
